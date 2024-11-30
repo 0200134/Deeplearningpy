eb30b04efab36d1e3f44d1bd21aa486941821bbe
0200134 <84560987+0200134@users.noreply.github.com>
2024-11-30 19:20:28 +0900

    Add files via upload

---
+++ AI11.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Embedding, MultiHeadAttention, LayerNormalization, Dropout
+from tensorflow.keras.models import Model
+import numpy as np
+
+# Positional Encoding
+def positional_encoding(position, d_model):
+    angle_rads = np.arange(position)[:, np.newaxis] / np.power(10000, (2 * (np.arange(d_model)[np.newaxis, :] // 2)) / np.float32(d_model))
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+class TransformerBlock(tf.keras.layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.ffn = tf.keras.Sequential([
+            Dense(ff_dim, activation="relu"),
+            Dense(embed_dim),
+        ])
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.dropout1 = Dropout(rate)
+        self.dropout2 = Dropout(rate)
+        
+    def call(self, inputs, training):
+        attn_output = self.att(inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+class TokenAndPositionEmbedding(tf.keras.layers.Layer):
+    def __init__(self, maxlen, vocab_size, embed_dim):
+        super(TokenAndPositionEmbedding, self).__init__()
+        self.token_emb = Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = Embedding(input_dim=maxlen, output_dim=embed_dim)
+    
+    def call(self, x):
+        maxlen = tf.shape(x)[-1]
+        positions = tf.range(start=0, limit=maxlen, delta=1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions
+
+def create_model(maxlen, vocab_size, embed_dim, num_heads, ff_dim):
+    inputs = tf.keras.Input(shape=(maxlen,))
+    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+    x = embedding_layer(inputs)
+    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    x = transformer_block(x)
+    x = tf.keras.layers.GlobalAveragePooling1D()(x)
+    x = Dropout(0.1)(x)
+    x = Dense(20, activation="relu")(x)
+    x = Dropout(0.1)(x)
+    outputs = Dense(2, activation="softmax")(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+# Parameters
+maxlen = 200  # Maximum number of words in a sequence
+vocab_size = 20000  # Vocabulary size
+embed_dim = 32  # Embedding dimension
+num_heads = 2  # Number of attention heads
+ff_dim = 32  # Hidden layer size in feed forward network
+
+# Create the model
+model = create_model(maxlen, vocab_size, embed_dim, num_heads, ff_dim)
+
+# Compile and summarize the model
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+model.summary()
