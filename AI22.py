853a29993bf5b2b9d465c73b43f2e2ea7673d38b
0200134 <84560987+0200134@users.noreply.github.com>
2024-11-30 19:45:31 +0900

    Add files via upload

---
+++ AI22.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+class PositionalEncoding(layers.Layer):
+    def __init__(self, position, d_model):
+        super(PositionalEncoding, self).__init__()
+        self.pos_encoding = self.add_weight(
+            "positional_encoding",
+            shape=(1, position, d_model),
+            initializer="zeros"
+        )
+
+    def call(self, x):
+        x = x * tf.math.sqrt(tf.cast(self.pos_encoding.shape[-1], tf.float32))
+        x = x + self.pos_encoding[:, :tf.shape(x)[1], :]
+        return x
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                           np.arange(d_model)[np.newaxis, :],
+                           d_model)
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
