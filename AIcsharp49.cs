cd67946d98833e88ebb075ba158374b5036cf4f0
0200134 <84560987+0200134@users.noreply.github.com>
2024-12-02 18:27:36 +0900

    Create AIcsharp49.cs

---
+++ AIcsharp49.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Image;
+using TensorFlow;
+
+namespace YOLOObjectDetection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mlContext = new MLContext();
+
+            // Load the image data
+            var dataPath = "path/to/your/imagefolder";
+            var imageData = new List<ImageData>
+            {
+                new ImageData { ImagePath = $"{dataPath}/image1.jpg" },
+                new ImageData { ImagePath = $"{dataPath}/image2.jpg" }
+            };
+            var dataView = mlContext.Data.LoadFromEnumerable(imageData);
+
+            // Define the image preprocessing pipeline
+            var pipeline = mlContext.Transforms.LoadImages("ImagePath", nameof(ImageData.ImagePath))
+                .Append(mlContext.Transforms.ResizeImages("ImagePath", imageWidth: 416, imageHeight: 416))
+                .Append(mlContext.Transforms.ExtractPixels("ImagePath"))
+                .Append(mlContext.Transforms.CopyColumns("ImagePath", "input_tensor"))
+                .Append(mlContext.Model.LoadTensorFlowModel("path/to/your/yolo_model")
+                    .AddInput("input_tensor")
+                    .AddOutput("output_tensor"));
+
+            // Train the model (if applicable)
+            var model = pipeline.Fit(dataView);
+
+            // Create the prediction engine
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ImageData, ImagePrediction>(model);
+
+            // Make predictions
+            var prediction = predictionEngine.Predict(new ImageData { ImagePath = $"{dataPath}/testImage.jpg" });
+
+            Console.WriteLine($"Detected objects: {prediction.NumDetections}");
+            for (int i = 0; i < prediction.NumDetections; i++)
+            {
+                Console.WriteLine($"Object {i + 1}: {prediction.DetectionClasses[i]} with score {prediction.DetectionScores[i]}");
+            }
+        }
+
+        public class ImageData
+        {
+            public string ImagePath { get; set; }
+        }
+
+        public class ImagePrediction
+        {
+            [ColumnName("detection_boxes")]
+            public float[,] DetectionBoxes { get; set; }
+
+            [ColumnName("detection_scores")]
+            public float[] DetectionScores { get; set; }
+
+            [ColumnName("detection_classes")]
+            public float[] DetectionClasses { get; set; }
+
+            [ColumnName("num_detections")]
+            public float NumDetections { get; set; }
+        }
+    }
+}
